bb7a747b645bb401b06d889cfbfc5c3ef4779097
justforvan <justforvangroup@gmail.com>
2023-02-11 17:30:03 +0800

    P

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-
+      <div className='grid h-screen place-items-center'>
+      <div className="card lg:card-side bg-base-100 shadow-xl">
+      <figure><img src="/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album"/></figure>
+      <div className="card-body">
+       <h2 className="card-title">New album is released!</h2>
+    <p>Click the button to listen on Spotiwhy app.</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Listen</button>
+    </div>
+  </div>
+</div>
+      </div>
       </main>
     </>
   )
